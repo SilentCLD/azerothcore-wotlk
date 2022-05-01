380d9803516567a5fb0cbbef2a128cb5711eec16
SilentCLD <sean@amaranthgaming.com>
2022-05-01 19:10:02 +0100

    Change sql file to new format

--- data/sql/updates/pending_db_world/rev_1649283775756264700.sql
+++ data/sql/updates/pending_db_world/rev_1649283775756264700.sql
@@ -1,5 +1,3 @@
-INSERT INTO `version_db_world` (`sql_rev`) VALUES ('1649283775756264700');
-
 -- Remove World Trigger that is interfering with the Wintergrasp NW tower teleporter
 DELETE FROM `creature` WHERE `guid` = 131721;
 DELETE FROM `creature_addon` WHERE `guid` = 131721;
